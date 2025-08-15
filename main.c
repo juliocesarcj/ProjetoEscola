8848ed44e7775807eabd4d497cd72d8af23f9b8f
juliocesarcj <juliojesus318@gmail.com>
2025-08-15 20:03:41 +0000

    restart

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int selecao=0;
+
+    while(selecao!=0){
+        printf("Menu:\n"
+            "0. Exit\n"
+            "1. Criar\n"
+            "2. Ler 2\n"
+            "3. atualizar\n"
+            "4. Deletar\n");
+        scanf("%d", &selecao);
+
+        switch(selecao){
+            case 1:
+
+        }
+    }
+}
